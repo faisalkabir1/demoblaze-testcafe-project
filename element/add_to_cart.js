6744468e4b63de1444b200bfaee9de3efe2650b4
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-02-22 21:29:09 +0600

    Update add_to_cart.js

--- element/add_to_cart.js
+++ element/add_to_cart.js
@@ -3,17 +3,18 @@ import { faker } from '@faker-js/faker';
 class AddtoCart {
     constructor() {
         this.product = Selector("a").withText("Samsung galaxy s6");
-        this.addtocart = Selector("a").withText("Add to cart");
+        this.addtocartbtn = Selector("a").withText("Add to cart");
         this.cartbtn = Selector("a").withText("Cart");
 
 
     }
     async addtoCartMethod() {
         await t.click(this.product)
-        await t.click(this.addtocart)
+        await t.click(this.addtocartbtn)
         await t.click(this.cartbtn)
     }
     async OrderMethod() {
 
     }
 }
+export default AddtoCart;
