a0dad31fb6305af6b95b126e89f891f1cad0f48b
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-02-22 21:46:54 +0600

    added order method pom

--- element/add_to_cart.js
+++ element/add_to_cart.js
@@ -5,7 +5,7 @@ class AddtoCart {
         this.product = Selector("a").withText("Samsung galaxy s6");
         this.addtocartbtn = Selector("a").withText("Add to cart");
         this.cartbtn = Selector("a").withText("Cart");
-
+        this.orderbtn = Selector("button").withText("Place Order");
 
     }
     async addtoCartMethod() {
@@ -14,6 +14,13 @@ class AddtoCart {
         await t.click(this.cartbtn)
     }
     async OrderMethod() {
+        await t.click(this.orderbtn)
+        await t.typeText("#name", "fkkabir");
+        await t.typeText("#country", "Bangladesh");
+        await t.typeText("#city", "Dhaka");
+        await t.typeText("#month", "12");
+        await t.typeText("#card", "1234567891011");
+        await t.typeText("#year", "2025")
 
     }
 }
